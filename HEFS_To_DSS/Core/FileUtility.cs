30802e54555faa750ef004af4793df020a1de8ef
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2021-04-22 15:09:32 -0700

    add more files

---
+++ HEFS_To_DSS/Core/FileUtility.cs
@@ -0,0 +1,475 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.Collections.Specialized;
+using System.Reflection;
+//using System.Windows.Forms;
+
+namespace Reclamation.Core
+{
+  public static class FileUtility
+  {
+    static List<string> fileList = new List<string>();
+    /// <summary>
+    /// Gets list of files
+    /// </summary>
+    /// <param name="directoryName"></param>
+    /// <returns></returns>
+    public static string[] GetFilesRecursive(string directoryName)
+    {
+      return GetFilesRecursive(directoryName, new string[] { });
+    }
+
+
+    /// <summary>
+    /// returns a list of files.
+    /// </summary>
+    /// <param name="directoryName">starting path to recurse</param>
+    /// <param name="extensionsToExclude">extensions to skip (includes leading .)</param>
+    /// <returns></returns>
+    public static string[] GetFilesRecursive(string directoryName, string[] extensionsToExclude)
+    {
+      fileList.Clear();
+      List<string> finalList = new List<string>();
+      if (Directory.Exists(directoryName))
+      {
+        ProcessDirectory(directoryName);
+      }
+      // remove files with extensions in exclude list.
+      for (int i = 0; i < fileList.Count; i++)
+      {
+        string file = fileList[i].ToString();
+        string ext = Path.GetExtension(file);
+        bool keepFile = true;
+        for (int j = 0; j < extensionsToExclude.Length; j++)
+        {
+          if (extensionsToExclude[j].ToLower() == ext.ToLower())
+          {
+            keepFile = false;
+            break;
+          }
+        }
+        if (keepFile)
+        {
+          finalList.Add(file);
+        }
+      }
+
+
+
+      fileList.Clear();
+      return finalList.ToArray();
+    }
+
+    /// <summary>
+    ///Process all files in the directory passed in, recurse on any directories  
+    ///that are found, and process the files they contain.
+    /// </summary>
+    /// <param name="targetDirectory"></param>
+    private static void ProcessDirectory(string targetDirectory)
+    {
+      // Process the list of files found in the directory.
+      string[] fileEntries = Directory.GetFiles(targetDirectory);
+      foreach (string fileName in fileEntries)
+        fileList.Add(fileName);
+
+      // Recurse into subdirectories of this directory.
+      string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+      foreach (string subdirectory in subdirectoryEntries)
+        ProcessDirectory(subdirectory);
+    }
+
+    public static void CopyFiles(string srcPath, string destPath, string searchPattern = "*.*", bool overwrite = true,
+        bool preserveAttributes = false)
+    {
+      string[] files = Directory.GetFiles(srcPath, searchPattern);
+      foreach (string srcFile in files)
+      {
+        string destFile = Path.Combine(destPath, Path.GetFileName(srcFile));
+        if (!overwrite && File.Exists(destFile))
+        {
+          // skip copy since file exists.
+        }
+        else
+        {
+          File.Copy(srcFile, destFile, overwrite);
+          File.SetCreationTime(destFile, File.GetCreationTime(srcFile));
+          File.SetLastAccessTime(destFile, File.GetLastAccessTime(srcFile));
+          File.SetLastWriteTime(destFile, File.GetLastWriteTime(srcFile));
+        }
+      }
+    }
+
+    /// <summary>
+    /// Gets a application specific temporay path
+    /// </summary>
+    /// <returns></returns>
+    public static string GetTempPath()
+    {
+      string s = Path.GetTempPath();
+      var asm = Assembly.GetEntryAssembly();
+      string n = "";
+      if (asm != null)
+        n = asm.GetName().Name;
+      else
+        n = Assembly.GetCallingAssembly().GetName().Name;
+
+      s = Path.Combine(s, "Reclamation");
+      s = Path.Combine(s, n);
+      if (!Directory.Exists(s))
+      {
+        Directory.CreateDirectory(s);
+      }
+
+      return s;
+    }
+
+
+    public static void CleanTempPath()
+    {
+
+      string path = GetTempPath();
+      //Logger.WriteLine("temp path = '" + path + "'");
+      try
+      {
+        string[] files = Directory.GetFiles(path);
+        foreach (string f in files)
+        {
+         // Logger.WriteLine("Deleting " + f + " from cache");
+          File.Delete(f);
+        }
+      }
+      catch (Exception)
+      {
+      }
+
+    }
+
+    public static string GetExecutableDirectory()
+    {
+      var asm = System.Reflection.Assembly.GetEntryAssembly();
+      if (asm == null)
+        return "";
+      var location = asm.Location;
+      //var location = System.AppContext.BaseDirectory;
+      var directory = System.IO.Path.GetDirectoryName(location);
+      return directory;
+      //return Path.GetDirectoryName(Application.ExecutablePath);
+    }
+
+    /// <summary>
+    /// C:\Users\UserName\AppData\Local\Reclamation\ProductName  (Windows 7)
+    /// </summary>
+    /// <returns></returns>
+    public static string GetLocalApplicationPath()
+    {
+      string s = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+      string n = Assembly.GetEntryAssembly().GetName().Name;
+
+      s = Path.Combine(s, "Reclamation");
+      s = Path.Combine(s, n);
+      if (!Directory.Exists(s))
+      {
+        Directory.CreateDirectory(s);
+      }
+
+      return s;
+
+    }
+    public static string GetSimpleTempFileName(string extension)
+    {
+      string path = GetTempPath();
+
+      var part1 = Guid.NewGuid().ToString("N").Substring(0, 8).ToLower();
+      var fn = Path.Combine(path, part1 + extension);
+
+      if (File.Exists(fn))
+        throw new Exception("File allready exists");
+
+      File.Create(fn).Close();
+
+      return fn;
+    }
+
+
+    /// <summary>
+    /// create a unique temporary filename with specified extension
+    /// in a temporary directory
+    /// <param name="extension">extension includes leading .</param>
+    /// </summary>
+    public static string GetTempFileName(string extension)
+    {
+      string path = GetTempPath();
+      return GetTempFileNameInDirectory(path, extension);
+    }
+
+    public static string GetTempFileNameInDirectory(string path, string extension)
+    {
+      return GetTempFileNameInDirectory(path, extension, "temp");
+    }
+
+    /// <summary>
+    /// create unique temporary filename in the specfied directory
+    /// </summary>
+    public static string GetTempFileNameInDirectory(string path, string extension, string filePrefix)
+    {
+      // https://stackoverflow.com/questions/4657974/how-to-generate-unique-file-names-in-c-sharp
+      var fn = filePrefix + Guid.NewGuid().ToString().ToLower() + extension;//".tmp";
+      fn = Path.Combine(path, fn);
+      File.Create(fn).Close();
+      return fn;
+    }
+
+
+    /// <summary>
+    /// read file and convert to array of bytes
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <returns></returns>
+    public static byte[] ReadFileAsBytes(string filename)
+    {
+      FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+      BinaryReader br = new BinaryReader(fs);
+
+      byte[] asm = br.ReadBytes((int)fs.Length);
+
+      br.Close();
+      fs.Close();
+
+      return asm;
+    }
+
+    /// <summary>
+    /// Read a file into memory and return a hash.
+    /// used to identify a specific version of a file.
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <returns></returns>
+    public static string GetSha1Hash(string filename)
+    {
+      FileStream f = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read, 8192);
+      SHA1CryptoServiceProvider sha1 = new SHA1CryptoServiceProvider();
+      sha1.ComputeHash(f);
+      f.Close();
+
+      byte[] hash = sha1.Hash;
+      StringBuilder buff = new StringBuilder();
+      foreach (byte hashByte in hash)
+      {
+        buff.Append(String.Format("{0:X1}", hashByte));
+      }
+      string rval = buff.ToString();
+      return rval;
+    }
+
+
+    /// <summary>
+    /// Creates a relative path from one file
+    /// or folder to another.
+    /// </summary>
+    /// <param name="fromDirectory">
+    /// Contains the directory that defines the 
+    /// start of the relative path.
+    /// </param>
+    /// <param name="toPath">
+    /// Contains the path that defines the
+    /// endpoint of the relative path.
+    /// </param>
+    /// <returns>
+    /// The relative path from the start
+    /// directory to the end path.
+    /// </returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static string RelativePathTo(
+        string fromDirectory, string toPath)
+    {
+      if (fromDirectory == null)
+        throw new ArgumentNullException("fromDirectory");
+      if (toPath == null)
+        throw new ArgumentNullException("toPath");
+
+      bool isRooted = Path.IsPathRooted(fromDirectory)
+                        && Path.IsPathRooted(toPath);
+
+      if (isRooted)
+      {
+        bool isDifferentRoot = string.Compare(
+
+            Path.GetPathRoot(fromDirectory),
+
+            Path.GetPathRoot(toPath), true) != 0;
+
+
+
+        if (isDifferentRoot)
+
+          return toPath;
+
+      }
+
+
+
+      StringCollection relativePath = new StringCollection();
+
+      string[] fromDirectories = fromDirectory.Split(
+
+          Path.DirectorySeparatorChar);
+
+
+
+      string[] toDirectories = toPath.Split(
+
+          Path.DirectorySeparatorChar);
+
+
+
+      int length = Math.Min(
+
+          fromDirectories.Length,
+
+          toDirectories.Length);
+
+
+
+      int lastCommonRoot = -1;
+
+
+
+      // find common root
+
+      for (int x = 0; x < length; x++)
+      {
+
+        if (string.Compare(fromDirectories[x],
+
+            toDirectories[x], true) != 0)
+
+          break;
+
+
+
+        lastCommonRoot = x;
+
+      }
+
+      if (lastCommonRoot == -1)
+
+        return toPath;
+
+
+
+      // add relative folders in from path
+
+      for (int x = lastCommonRoot + 1; x < fromDirectories.Length; x++)
+
+        if (fromDirectories[x].Length > 0)
+
+          relativePath.Add("..");
+
+
+
+      // add to folders to path
+
+      for (int x = lastCommonRoot + 1; x < toDirectories.Length; x++)
+
+        relativePath.Add(toDirectories[x]);
+
+
+
+      // create relative path
+
+      string[] relativeParts = new string[relativePath.Count];
+
+      relativePath.CopyTo(relativeParts, 0);
+
+
+
+      string newPath = string.Join(
+
+          Path.DirectorySeparatorChar.ToString(),
+
+          relativeParts);
+
+
+
+      return newPath;
+
+    }
+
+    public static bool FilesAreEqual(string file1, string file2)
+    {
+      return GetSha1Hash(file1) == GetSha1Hash(file2);
+    }
+
+
+
+    /// <summary>
+    /// Returns true if the file is less than 20 minutes old
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <returns></returns>
+    public static bool IsFileNewEnough(string filename)
+    {
+      if (!File.Exists(filename))
+        return false;
+
+      FileInfo fi = new FileInfo(filename);
+      // int minutes = GetCacheAllowedMinutes();
+      bool rval = fi.CreationTime.AddMinutes(20) > DateTime.Now;
+      return rval;
+    }
+
+    /// <summary>
+    /// Gets the full path to a file using the following preference order:
+    /// 1) exe path
+    /// 2) exe/cfg path 
+    /// 3) Globals.CfgDataPath (pisces/hydromet/cfg)
+    /// 3) Globals.LocalConfigurationDataPath  (set in app.config) 
+    /// 4) Root of the project source folder: System.AppDomain.CurrentDomain.BaseDirectory
+    /// </summary>
+    /// <param name="filename"></param>
+    /// <returns></returns>
+    public static string GetFileReference(string filename)
+    {
+      // Build list of potential paths to look for file at
+      var list = new LinkedList<string>();
+      list.AddLast(Path.Combine(GetExecutableDirectory(), filename));
+      list.AddLast(Path.Combine(GetExecutableDirectory(), "cfg", filename));
+      
+      foreach (string path in list)
+      {
+        if (File.Exists(path))
+        {
+          //Logger.WriteLine("found file in pisces root " + path);
+          return path;
+        }
+      }
+
+      // Case where file not found.
+     // Logger.WriteLine("Warning: could not find " + filename);
+      throw new FileNotFoundException(filename);
+    }
+
+    /// <summary>
+    /// Move a file to sub directory, creating sub directory as needed.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="subdirectory"></param>
+    /// <param name="file"></param>
+    public static void MoveToSubDirectory(string path, string subdirectory, string file)
+    {
+      var attic = Path.Combine(path, subdirectory);
+
+      if (!Directory.Exists(attic))
+        Directory.CreateDirectory(attic);
+
+      string dest = Path.Combine(attic, Path.GetFileName(file));
+      if (File.Exists(dest))
+        File.Delete(dest);
+
+      File.Move(file, dest);
+    }
+  }
+}
---
+++ HEFS_To_DSS/Hec.Ensemble/HEFS_WebReader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hec.Ensemble
+{
+  public class HEFS_WebReader
+  {
+    public static void Read(string watershedName, DateTime ForecastDate, string outputDir)
+    {
+      string _rootUrl = "https://www.cnrfc.noaa.gov/csv/";
+      //https://www.cnrfc.noaa.gov/csv/2019092312_RussianNapa_hefs_csv_hourly.zip
+      string webrequest = _rootUrl;
+
+      string fileName = ForecastDate.ToString("yyyyMMddhh") + "_";
+      fileName += watershedName;
+      fileName += "_hefs_csv_hourly";
+      webrequest += fileName + ".zip";
+
+      string zipFileName = Path.Combine(outputDir, fileName + ".zip");
+      string csvFileName = Path.Combine(outputDir, fileName + ".csv");
+      if (File.Exists(csvFileName))
+      {
+        Console.WriteLine("Found " + csvFileName + " in cache.  skipping...");
+        return;
+      }
+
+      if (!File.Exists(zipFileName))
+      {
+        try
+        {
+          Console.WriteLine("downloading " + zipFileName);
+          GetFile(webrequest, zipFileName);
+        }
+        catch (System.Net.WebException ex)
+        {
+          Console.WriteLine(ex.Message);
+          Console.WriteLine("404, " + zipFileName + " not found ");
+          return;
+        }
+      }
+      Reclamation.Core.ZipFileUtility.UnzipFile(zipFileName, csvFileName);
+    }
+
+
+
+
+    /// <summary>
+    /// from https://stackoverflow.com/questions/137285/what-is-the-best-way-to-read-getresponsestream
+    /// </summary>
+    static void GetFile(string url, string outputFilename)
+    {
+      HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(url);
+      httpRequest.Method = "GET";
+
+      // if the URI doesn't exist, an exception will be thrown here...
+      using (HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse())
+      {
+        using (Stream responseStream = httpResponse.GetResponseStream())
+        {
+          using (FileStream localFileStream =
+              new FileStream(outputFilename, FileMode.Create))
+          {
+            var buffer = new byte[4096];
+            long totalBytesRead = 0;
+            int bytesRead;
+
+            while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+              totalBytesRead += bytesRead;
+              localFileStream.Write(buffer, 0, bytesRead);
+            }
+          }
+        }
+      }
+    }
+  }
+}
