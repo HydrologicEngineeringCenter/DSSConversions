2b09b052c01d544e33001e30b3d1b2db72d0f692
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2021-04-23 08:51:54 -0700

    fix -- read range of forecast dates.

--- HEFS_To_DSS/Program.cs
+++ HEFS_To_DSS/Program.cs
@@ -18,18 +18,28 @@ namespace HEFS_To_DSS
         Console.WriteLine("Example: HEFS_To_DSS.exe RussianNapa \"2021-04-22 12:00\" \"2021-04-22 12:00\" c:\\temp\\downloads output.dss");
 
         Console.WriteLine("downloads HEFS data and imports to DSS");
+        return;
       }
-      // read from web.
+
       var watershedName = args[0]; // "RussianNapa";
-      var t = DateTime.Parse(args[1]); //new DateTime(2019, 9, 23, 12, 0, 0);
+      var t1 = DateTime.Parse(args[1]); //new DateTime(2019, 9, 23, 12, 0, 0);
       var t2 = DateTime.Parse(args[2]); //new DateTime(2019, 9, 23, 12, 0, 0);
       var csvDir = args[3]; // @"c:\temp\downloads";
       var dssFileName = args[4];
+
+      var t = t1;
+      while (t <= t2)
+      {
+         HEFS_WebReader.Read(watershedName, t, csvDir);
+        t = t.AddDays(1);
+      }
+
       HEFS_WebReader.Read(watershedName, t, csvDir);
 
-      // import
+      // read csv into memory
       var er = new CsvEnsembleReader(csvDir);
-      var ws = er.Read(watershedName, t, t2);
+      var ws = er.Read(watershedName, t1, t2);
+      // write to DSS file
       DssEnsemble.Write(dssFileName, ws);
       
 
